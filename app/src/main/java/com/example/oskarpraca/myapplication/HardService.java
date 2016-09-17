3a51d0670b59fd1f60a0d924d3449309a7518510
vallerboy <oskarbank@gmail.com>
2016-09-17 11:01:44 +0200

    test

--- app/src/main/java/com/example/oskarpraca/myapplication/HardService.java
+++ app/src/main/java/com/example/oskarpraca/myapplication/HardService.java
@@ -3,14 +3,19 @@ package com.example.oskarpraca.myapplication;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 /**
  * Created by OskarPraca on 2016-09-17.
  */
 public class HardService extends Service {
 
+    Handler handler = new Handler();
+
+    private final IBinder mBinder = new LocalBinder();
 
     public class LocalBinder extends Binder {
             HardService getService() {
@@ -20,6 +25,16 @@ public class HardService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
+    }
+
+    public void createToast() {
+       handler.post(new Runnable() {
+           @Override
+           public void run() {
+               Toast.makeText(getApplicationContext(), "Ja znów z serwisu!", Toast.LENGTH_LONG).show();
+           }
+       });
+
     }
 }
